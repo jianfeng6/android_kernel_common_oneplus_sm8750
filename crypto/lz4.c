47e4bb951ab70700f6cd42dba72dacbd1f7791e2
brokestar233 <3765589194@qq.com>
2025-09-13 14:11:29 +0800

    lz4: Use ARM64 NEON-optimized decompression where available
    This commit adds conditional compilation for ARM64 platforms with
    CONFIG_KERNEL_MODE_NEON enabled, utilizing the optimized
    LZ4_arm64_decompress_safe() function for safe decompression in both
    LZ4 and LZ4HC crypto modules. This improves performance on ARM64
    systems by leveraging NEON instructions for faster decompression,
    while falling back to the generic LZ4_decompress_safe() on other
    architectures.
    
    The changes apply the optimization in:
      __lz4_decompress_crypto() in crypto/lz4.c
      __lz4hc_decompress_crypto() in crypto/lz4hc.c
    
    Signed-off-by: brokestar233 3765589194@qq.com

--- crypto/lz4.c
+++ crypto/lz4.c
@@ -173,7 +173,11 @@ static int __lz4_decompress_crypto(const u8 *src, unsigned int slen,
 
 		out_len = LZ4_decompress_safe_continue(stream, src, dst, slen, *dlen);
 	} else {
+#if defined(CONFIG_ARM64) && defined(CONFIG_KERNEL_MODE_NEON)
+		out_len = LZ4_arm64_decompress_safe(src, dst, slen, *dlen, false);
+#else
 		out_len = LZ4_decompress_safe(src, dst, slen, *dlen);
+#endif
 	}
 
 	if (out_len < 0)
